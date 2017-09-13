ca862c47e4867aa6f7c800ff0b40f982a4f34c92
James Hughes <hugjimbo77@gmail.com>
2017-09-13 16:36:39 +0100

    Entering players

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
 get '/' do
-  'Testing infrastructure working!'
+  erb :index
+end
+
+post '/names' do
+  @Player1 = params[:Player1]
+  @Player2 = params[:Player2]
+  erb :play
 end
 
 # start the server if ruby file executed directly
