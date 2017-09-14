def876b864e37b29e181989af4241b93759380d9
James Hughes <hugjimbo77@gmail.com>
2017-09-14 12:45:52 +0100

    refactored GET '/play'

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 
 get '/' do
   erb :index
 end
 
 post '/names' do
-  @Player1 = params[:Player1]
-  @Player2 = params[:Player2]
+  # @Player1 = params[:Player1]
+  session[:Player1] = params[:Player1]
+  session[:Player2] = params[:Player2]
+  redirect '/play'
+end
+
+get '/play' do
   erb :play
 end
 
